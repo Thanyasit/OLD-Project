22de67be2540f1162298b781b2063fe8efc97a91
Thanyasit <77733903+Thanyasit@users.noreply.github.com>
2023-03-22 09:42:04 +0700

    Delete 2019/Introduction Robot directory

--- 2019/Introduction Robot/README.md
+++
@@ -1,1 +0,0 @@
-# Studied at Suranaree University
--- 2019/Introduction Robot/TA1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-a =5
-b=6
-c=7
-d=np.rad2deg(np.arccos(c/(a*b)))
-print("D = ","%.2f"%(d)," Degrees")
-            
-
-            
